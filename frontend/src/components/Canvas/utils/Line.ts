ac5e2fd5ebdf9f7a01386afe211937b4aca45b46
jjiny0716 <jjiny3773@naver.com>
2022-11-17 13:31:25 +0900

    feat: 곡선 그릴 때 중복되는 성분 제거
    - 이전 점과 x 혹은 y좌표가 일치할 경우 현재 점을 추가하지 않고, 이전 점을 업데이트

--- frontend/src/components/Canvas/utils/Line.ts
+++ frontend/src/components/Canvas/utils/Line.ts
@@ -6,6 +6,28 @@ export default class Line extends Shape {
   protected points: Point[] = [];
 
   pushPoint(point: Point) {
+    const prevPoint = this.points.at(-1);
+    const prevPrevPoint = this.points.at(-2);
+
+    // 이전 점이 없을 시 그냥 push
+    if (!prevPoint || !prevPrevPoint) {
+      this.points.push(point);
+      return;
+    }
+
+    // 이전 점들과 x좌표가 동일하면, 바로 이전 점의 y좌표만 변경
+    if (point.x === prevPrevPoint.x && prevPoint.x === prevPrevPoint.x) {
+      prevPoint.y = point.y;
+      return;
+    }
+
+    // 이전 점들과 y좌표가 동일하면, 바로 이전 점의 x좌표만 변경
+    if (point.y === prevPrevPoint.y && prevPoint.y === prevPrevPoint.y) {
+      prevPoint.x = point.x;
+      return;
+    }
+
+    // 이전 점들과 공통된 성분이 없을 경우, 그냥 push
     this.points.push(point);
   }
 
@@ -24,5 +46,6 @@ export default class Line extends Shape {
     ctx.stroke(); 
 
     setCanvasContextSetting(ctx, prevCtxSetting);
+    console.log(this.points.length);
   }
 }
