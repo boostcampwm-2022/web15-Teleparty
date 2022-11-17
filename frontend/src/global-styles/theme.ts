8afc3c7f24402d8106e325033609b2296a468abe
m4nd4r1n <kara.mandarin99@gmail.com>
2022-11-17 13:40:14 +0900

    feat: 굵기 선택 컴포넌트 구현

--- frontend/src/global-styles/theme.ts
+++ frontend/src/global-styles/theme.ts
@@ -6,6 +6,7 @@ export const colors = {
   primaryDark: "#15164A",
   primaryLight: "#CCCDFF",
   primaryLightTransparent: "rgba(204,205,255,0.4)",
+  primaryTransparent: "rgba(33,34,105,0.4)",
 
   // black-white
   black: "#222222",
