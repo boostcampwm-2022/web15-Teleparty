f1b037215ae7f2b099cb62ec2fe5fa28330aac25
m4nd4r1n <kara.mandarin99@gmail.com>
2022-11-17 17:34:08 +0900

    feat: 그림판 되돌리기 구현
    - `Ctrl(Cmd) + z`를 입력하여 이전 상태로 되돌리기 구현

--- frontend/src/components/Canvas/Canvas.component.tsx
+++ frontend/src/components/Canvas/Canvas.component.tsx
@@ -7,11 +7,16 @@ import Shape from "./utils/Shape";
 
 import { getCoordRelativeToElement } from "../../utils/coordinate";
 
+const CANVAS_PROPS = {
+  WIDTH: 1036,
+  HEIGHT: 644,
+};
+
 const Canvas = () => {
-	const canvasRef = useRef<HTMLCanvasElement>(null);
-	const shapeList = useRef<Shape[]>([]);
-	const isDrawing = useRef<boolean>(false);
-	const lastPoint = useRef<Point>({ x: 0, y: 0 });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const shapeList = useRef<Shape[]>([]);
+  const isDrawing = useRef<boolean>(false);
+  const lastPoint = useRef<Point>({ x: 0, y: 0 });
 
   useEffect(() => {
     const ctx = canvasRef.current?.getContext("2d");
@@ -20,60 +25,71 @@ const Canvas = () => {
     ctx.lineJoin = "round";
   }, []);
 
-	const drawAllShapes = () => {
-		const ctx = canvasRef.current?.getContext("2d");
-		for (const shape of shapeList.current) {
-			ctx && shape.draw(ctx);
-		}
-	};
+  const drawAllShapes = () => {
+    const ctx = canvasRef.current?.getContext("2d");
+    for (const shape of shapeList.current) {
+      ctx && shape.draw(ctx);
+    }
+  };
 
-	const drawStart: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
-		shapeList.current.push(new Line("#aa22aa", 1, 10));
-		isDrawing.current = true;
-		lastPoint.current = getCoordRelativeToElement(
-			event.clientX,
-			event.clientY,
-			event.target as Element
-		);
-	};
+  const drawStart: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
+    shapeList.current.push(new Line("#aa22aa", 1, 10));
+    isDrawing.current = true;
+    lastPoint.current = getCoordRelativeToElement(
+      event.clientX,
+      event.clientY,
+      event.target as Element
+    );
+  };
 
-	const draw: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
-		if (!isDrawing.current) return;
+  const draw: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
+    if (!isDrawing.current) return;
 
-		const target = shapeList.current.at(-1);
+    const target = shapeList.current.at(-1);
     const currentPoint = getCoordRelativeToElement(
       event.clientX,
       event.clientY,
       event.target as Element
     );
 
-		if (target instanceof Line) {
+    if (target instanceof Line) {
       // 새로운 선을 추가
-			target.pushLine({
-				start: lastPoint.current,
-				end: currentPoint,
-			});
-		}
+      target.pushLine({
+        start: lastPoint.current,
+        end: currentPoint,
+      });
+    }
 
     lastPoint.current = currentPoint;
     drawAllShapes();
-	};
+  };
 
   const drawEnd: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
     draw(event);
     isDrawing.current = false;
-  }
+  };
 
-	return (
-		<CanvasLayout
-			width={1036}
-			height={644}
-			ref={canvasRef}
-			onMouseDown={drawStart}
-			onMouseMove={draw}
-			onMouseUp={drawEnd}
-		/>
-	);
+  const undo = (e: React.KeyboardEvent<HTMLCanvasElement>) => {
+    if ((e.metaKey || e.ctrlKey) && e.key === "z") {
+      const ctx = canvasRef.current?.getContext("2d");
+      shapeList.current.pop();
+      ctx?.clearRect(0, 0, CANVAS_PROPS.WIDTH, CANVAS_PROPS.HEIGHT);
+      drawAllShapes();
+    }
+  };
+
+  return (
+    <CanvasLayout
+      width={CANVAS_PROPS.WIDTH}
+      height={CANVAS_PROPS.HEIGHT}
+      ref={canvasRef}
+      onMouseDown={drawStart}
+      onMouseMove={draw}
+      onMouseUp={drawEnd}
+      onKeyDown={undo}
+      tabIndex={1}
+    />
+  );
 };
 
 export default Canvas;
