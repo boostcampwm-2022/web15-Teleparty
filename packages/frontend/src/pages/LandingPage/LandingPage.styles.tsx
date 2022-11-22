797467ba1bdb6fa5351603fa9fed8999ca1c74d3
jjiny0716 <jjiny3773@naver.com>
2022-11-22 16:08:39 +0900

    style: 랜딩 페이지 로고 가운데 정렬

--- packages/frontend/src/pages/LandingPage/LandingPage.styles.tsx
+++ packages/frontend/src/pages/LandingPage/LandingPage.styles.tsx
@@ -4,6 +4,8 @@ export const LandingPageLayout = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  gap: 100px;
   height: 100vh;
   background: ${(props) => props.theme.gradients.background};
 `;
