c40ff9ecccb96296132b973b5bf070abcbe3a580
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-11-22 17:35:58 +0900

    feat : 플레이어 join 이벤트 구현
    - 방 번호가 없으면 새로 생성
    - 방 번호에 해당하는 방이 있으면 해당 방에 접속

--- backend/src/domains/room/room.controller.ts
+++ backend/src/domains/room/room.controller.ts
@@ -1,4 +1,5 @@
 import { Socket } from "socket.io";
+import { io as socketServer } from "../../app";
 import { Player } from "../player/player";
 import { Room } from "./room.model";
 
@@ -7,16 +8,44 @@ const rooms: Room[] = []; // 나중에 redis에서 관리 하도록 변경
 export const roomController = (socket: Socket) => {
   socket.on("join", (data: { userName: string; roomId: string }) => {
     const player = new Player(socket, data.userName);
-    if (!data.roomId) {
-      const room = new Room("123123");
-      room.join(player);
-      rooms.push(room);
-    } else {
-      rooms.forEach((room) => {
-        if (room.roomId == data.roomId) {
-          room.join(player);
-        }
+
+    if (data.roomId) {
+      const room = rooms.find((room: Room) => {
+        return room.roomId === data.roomId;
       });
+
+      if (room) {
+        room.join(player);
+        socketServer.to(room.roomId).emit(
+          "join",
+          room.players.map((player: Player) => {
+            return {
+              userName: player.userName,
+              userId: player.socket.id,
+              avata: player.avata,
+              score: player.score,
+            };
+          })
+        );
+
+        return;
+      }
     }
+
+    const room = new Room("123123");
+    room.join(player);
+    rooms.push(room);
+
+    socketServer.to(room.roomId).emit(
+      "join",
+      room.players.map((player: Player) => {
+        return {
+          userName: player.userName,
+          userId: player.socket.id,
+          avata: player.avata,
+          score: player.score,
+        };
+      })
+    );
   });
 };
