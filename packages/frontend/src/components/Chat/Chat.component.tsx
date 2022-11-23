27d457e8e899594234a3bf5dff560f4a387582c5
m4nd4r1n <kara.mandarin99@gmail.com>
2022-11-23 13:45:44 +0900

    feat: 채팅 소켓 송수신 구현
    - socket.io-client 설치
    - Jotai atom으로 socket 관리

--- packages/frontend/src/components/Chat/Chat.component.tsx
+++ packages/frontend/src/components/Chat/Chat.component.tsx
@@ -1,14 +1,17 @@
 import { useEffect, useRef, useState } from "react";
 
+import { useAtomValue } from "jotai";
+
 import { ChatInputForm, ChatLayout } from "./Chat.styles";
 import ChatBubble from "./ChatBubble.component";
 
+import { socketAtom } from "../../store/socket";
 import { Button } from "../Button/Button.styles";
 import Icon from "../Icon/Icon";
 import { Input } from "../Input/Input.styles";
 
 interface ChatData {
-  username: string;
+  peerId: string;
   message: string;
 }
 
@@ -16,53 +19,35 @@ interface ChatProps {
   variant?: "horizontal";
 }
 
-const mock = [
-  { username: "me", message: "안녕하세요" },
-  {
-    username: "me",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare mattis orci. Vestibulum sed facilisis dolor.",
-  },
-  {
-    username: "other1",
-    message: "안녕하세요. 반갑습니다.",
-  },
-  {
-    username: "other1",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare mattis orci. Vestibulum sed facilisis dolor.",
-  },
-  {
-    username: "other2",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare mattis orci. Vestibulum sed facilisis dolor.",
-  },
-  {
-    username: "other2",
-    message:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare mattis orci. Vestibulum sed facilisis dolor.",
-  },
-];
-
 const Chat = ({ variant }: ChatProps) => {
-  const [chats, setChats] = useState<ChatData[]>(mock);
+  const [chats, setChats] = useState<ChatData[]>([]);
   const chatEndRef = useRef<HTMLDivElement>(null);
   const chatInputRef = useRef<HTMLInputElement>(null);
+  const socket = useAtomValue(socketAtom);
 
   const onChatSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!chatInputRef.current || !chatInputRef.current.value) return;
 
-    const newChatData = {
-      username: "me",
+    const newChat = {
       message: chatInputRef.current.value,
     };
-    setChats((prev) => [...prev, newChatData]);
 
     // socket emit
+    socket.emit("chatting", newChat);
 
     chatInputRef.current.value = "";
   };
+
+  useEffect(() => {
+    const chatListener = (newChat: ChatData) => {
+      setChats((prev) => [...prev, newChat]);
+    };
+    socket.on("chatting", chatListener);
+    return () => {
+      socket.off("chatting", chatListener);
+    };
+  }, []);
 
   useEffect(() => {
     chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -71,13 +56,13 @@ const Chat = ({ variant }: ChatProps) => {
   return (
     <>
       <ChatLayout variant={variant}>
-        {chats.map(({ username, message }, index, messages) => (
+        {chats.map(({ peerId, message }, index, messages) => (
           <ChatBubble
             key={index}
             variant={variant}
-            isMine={username === "me"}
-            isFirst={messages[index - 1]?.username !== username}
-            username={username}
+            isMine={peerId === socket.id}
+            isFirst={messages[index - 1]?.peerId !== peerId}
+            username={peerId} // user list 추가되면 변경 필요
           >
             {message}
           </ChatBubble>
