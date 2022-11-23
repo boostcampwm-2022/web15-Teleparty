a6955359a25dd6940210fae7ba65200084f27477
spaff13 <spaff316@gmail.com>
2022-11-23 21:37:17 +0900

    feat: 필요없는 파일 삭제

--- packages/backend/src/domains/game/outBound/gameRepository.port.ts
+++
@@ -1,7 +0,0 @@
-import { Game } from "../entity/game";
-
-export interface GameRepository {
-  save: (game: Game) => void;
-  findById: (id: string) => Game | null;
-  delete: (id: string) => void;
-}
