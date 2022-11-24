c6b8a234b09bbcbbb175e0da71eb1ebb5117e827
spaff13 <spaff316@gmail.com>
2022-11-24 15:29:37 +0900

    feat: 캐치마인드 아웃바운드 어댑터 구현

--- packages/backend/src/domains/game/outBound/CatchMindEvent.Adapter.ts
+++ packages/backend/src/domains/game/outBound/CatchMindEvent.Adapter.ts
@@ -6,27 +6,42 @@ import {
 } from "./catchMindEvent.port";
 
 import { SocketEmitter } from "../../../utils/socketEmitter";
+import { Player } from "../entity/catchMind";
 
 export class CatchMindEventAdapter implements CatchMindEvent {
-  roomId: string;
   emitter: SocketEmitter;
-  constructor(roomId: string) {
-    this.roomId = roomId;
+  constructor() {
     this.emitter = new SocketEmitter();
   }
-  gameStart(data: StartGameData) {
-    this.emitter.broadcastRoom(this.roomId, "game-start", data);
+
+  gameStart(roomId: string, { totalRound, roundInfo }: StartGameData) {
+    this.emitter.broadcastRoom(roomId, "game-start", {
+      gameMode: "catchMind",
+      totalRound,
+      roundInfo,
+    });
   }
-  drawStart(id: string) {
-    this.emitter.broadcastRoom(this.roomId, "draw-start", { id });
+
+  drawStart(roomId: string, { id }: Player) {
+    this.emitter.broadcastRoom(roomId, "draw-start", { peerId: id });
   }
-  roundEnd(data: RoundEndData) {
-    this.emitter.broadcastRoom(this.roomId, "round-end", data);
+
+  roundEnd(roomId: string, data: RoundEndData) {
+    const players = data.playerScoreList;
+
+    this.emitter.broadcastRoom(roomId, "round-end", {
+      ...data,
+      playerScoreList: players.map(({ id, score }) => {
+        return { peerId: id, score };
+      }),
+    });
   }
-  roundReady(id: string) {
-    this.emitter.broadcastRoom(this.roomId, "round-ready", { id });
+
+  roundReady(roomId: string, { id }: Player) {
+    this.emitter.broadcastRoom(roomId, "round-ready", { peerId: id });
   }
-  roundStart(data: CatchMindInfo) {
-    this.emitter.broadcastRoom(this.roomId, "round-start", data);
+
+  roundStart(roomId: string, data: CatchMindInfo) {
+    this.emitter.broadcastRoom(roomId, "round-start", data);
   }
 }
