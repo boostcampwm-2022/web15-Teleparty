0002d0a430b68b36b5830f4513575cf83913b8bc
spaff13 <spaff316@gmail.com>
2022-11-24 14:36:34 +0900

    feat: chat 아웃바운드 어댑터 구현

---
+++ packages/backend/src/domains/chat/outbound/chatOut.adapter.ts
@@ -0,0 +1,9 @@
+import { SocketEmitter } from "../../../utils/socketEmitter";
+import { ChatOutPort } from "./chatOut.port";
+
+export class ChatOutAdapter implements ChatOutPort {
+  emitter: SocketEmitter = new SocketEmitter();
+  broadCast(roomId: string, data: { message: string; id: string }) {
+    this.broadCast(roomId, data);
+  }
+}
