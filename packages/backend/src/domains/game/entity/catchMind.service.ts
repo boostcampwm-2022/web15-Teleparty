984974d57f39c61e7b86f8d37badf7196294d503
spaff13 <spaff316@gmail.com>
2022-11-29 18:04:30 +0900

    feat: 게임 중 나갔을 때 로직 설정
    - 게임중 나갔을 때 게임 엔티티 플레이어 리스트에서 제거
    - 나간 사람이 턴 플레이어일 시 라운드 종료

--- packages/backend/src/domains/game/entity/catchMind.service.ts
+++ packages/backend/src/domains/game/entity/catchMind.service.ts
@@ -108,4 +108,18 @@ export class CatchMindService implements CatchMindInputPort {
     this.eventEmitter.roundStart(game.roomId, game.roundInfo);
     this.repository.save(game);
   }
+
+  quitGame(roomId: string, playerId: string) {
+    const game = this.repository.findById(roomId);
+    if (!game) return;
+
+    // 여기서 신호
+    if (game.turnPlayer.id === playerId) {
+      clearTimeout(game.timerId);
+      this.roundEnd(game, null);
+    }
+    game.removePlayer(playerId);
+
+    this.repository.save(game);
+  }
 }
