6108a703e1b22e1c1280d8578485482779df459e
jjiny0716 <jjiny3773@naver.com>
2022-11-29 21:50:43 +0900

    refactor: useAudioCommunication 커스텀 훅 리팩토링
    - 중복 코드 함수화
    - 주석 추가
    - 등록한 이벤트리스너와 MediaConnection들을 제거하는 클린업 함수 구현 및 적용

--- packages/frontend/src/hooks/useAudioCommunication.tsx
+++ packages/frontend/src/hooks/useAudioCommunication.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import Peer, { MediaConnection } from "peerjs";
+
+const getAudioMediaStream = () => {
+  return navigator.mediaDevices.getUserMedia({
+    video: false,
+    audio: true,
+  });
+};
 
 export const useAudioCommunication = (
   peer: Peer | null,
   peerIdList: string[]
 ) => {
+  const mediaConnectionSet = useRef<Set<MediaConnection>>(new Set());
+
   const initMediaConnection = (mediaConnection: MediaConnection) => {
+    mediaConnectionSet.current.add(mediaConnection);
+
     mediaConnection.on("stream", (stream) => {
       const audio = new Audio();
       audio.autoplay = true;
@@ -14,45 +25,59 @@ export const useAudioCommunication = (
     });
 
     mediaConnection.on("close", () => {
-      console.log("call closed");
+      mediaConnectionSet.current.delete(mediaConnection);
     });
 
     mediaConnection.on("error", (error) => {
+      mediaConnectionSet.current.delete(mediaConnection);
       console.error(error);
     });
   };
 
-  const initPeer = async () => {
-    if (!peer) return;
-
-    const audioStream = await navigator.mediaDevices.getUserMedia({
-      video: false,
-      audio: true,
-    });
-
-    peer.on("call", (mediaConnection) => {
-      mediaConnection.answer(audioStream);
-      initMediaConnection(mediaConnection);
-    });
+  // answer to call and handle new MediaConnection
+  const handleCall = async (mediaConnection: MediaConnection) => {
+    const audioStream = await getAudioMediaStream();
+    mediaConnection.answer(audioStream);
+    initMediaConnection(mediaConnection);
   };
 
-  const connectRtcToRoomPlayers = async () => {
+  const initPeer = async () => {
+    if (!peer) return;
+    peer.on("call", handleCall);
+  };
+
+  const clearPeer = () => {
+    if (!peer) return;
+    peer.off("call", handleCall);
+  };
+
+  // call to all peers and handle each new MediaConnection
+  const connectAudioWithPeers = async () => {
     if (!peer) return;
 
-    for (const peerId of peerIdList) {
-      const mediaStream = await navigator.mediaDevices.getUserMedia({
-        video: false,
-        audio: true,
-      });
+    const audioStream = await getAudioMediaStream();
 
-      const mediaConnection = peer.call(peerId, mediaStream);
+    for (const peerId of peerIdList) {
+      const mediaConnection = peer.call(peerId, audioStream);
       initMediaConnection(mediaConnection);
     }
   };
 
+  const closeAllMediaConnections = () => {
+    for (const mediaConnection of mediaConnectionSet.current) {
+      mediaConnection.close();
+    }
+  };
+
+  // 1. init peer(WebRTC) to accept incoming audio connection
+  // 2. connect audio channel with all peers
   useEffect(() => {
-    if (!peer) return;
     initPeer();
-    connectRtcToRoomPlayers();
+    connectAudioWithPeers();
+
+    return () => {
+      clearPeer();
+      closeAllMediaConnections();
+    };
   }, []);
 };
