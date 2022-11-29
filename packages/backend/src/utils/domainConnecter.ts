5485e75cc9ffe7b04de70a97636ff444eca3906e
spaff13 <spaff316@gmail.com>
2022-11-30 02:25:25 +0900

    feat: 커넥터에서 데이터를 넣지 않아도 되게 수정

--- packages/backend/src/utils/domainConnecter.ts
+++ packages/backend/src/utils/domainConnecter.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export class DomainConnecter {
   static instance: DomainConnecter;
-  controllerMap: Map<string, (...arg0: any[]) => any> = new Map();
+  controllerMap: Map<string, (data?: any) => any> = new Map();
 
   static getInstance(): DomainConnecter {
     if (DomainConnecter.instance) {
@@ -12,11 +12,11 @@ export class DomainConnecter {
     return DomainConnecter.instance;
   }
 
-  register(controllerName: string, controller: (...arg0: any[]) => any) {
+  register(controllerName: string, controller: (data?: any) => any) {
     this.controllerMap.set(controllerName, controller);
   }
 
-  call(controllerName: string, data: any) {
+  call(controllerName: string, data?: any) {
     const controller = this.controllerMap.get(controllerName);
 
     if (controller) {
