ef8e339d62e83e171121f74d4c127c7d5573dea5
spaff13 <spaff316@gmail.com>
2022-11-30 16:24:44 +0900

    feat: 레포지토리 제작
    Co-authored-by: youhavetopay <youhavetopay@users.noreply.github.com>

---
+++ packages/backend/src/domains/garticphone/outbound/garticphone.repository.ts
@@ -0,0 +1,16 @@
+import { Garticphone } from "../entity/garticphone";
+import { GarticphoneRepositoryDataPort } from "./garitc.port";
+
+export class GarticphoneRepository implements GarticphoneRepositoryDataPort {
+  static games: Map<string, Garticphone> = new Map();
+
+  save(game: Garticphone) {
+    GarticphoneRepository.games.set(game.roomId, game);
+  }
+  findById(id: string) {
+    return GarticphoneRepository.games.get(id) || undefined;
+  }
+  delete(roomId: string) {
+    GarticphoneRepository.games.delete(roomId);
+  }
+}
