c164ff6bbcc83f2d6cfd1a44985af90e9ae25f7a
spaff13 <spaff316@gmail.com>
2022-11-30 13:59:52 +0900

    feat: room 도메인에서 player-quit이벤트를 받지 않도록 처리

--- packages/backend/src/domains/player/inbound/player.controller.ts
+++ packages/backend/src/domains/player/inbound/player.controller.ts
@@ -52,9 +52,9 @@ router.get("disconnect", (socket: Socket) => {
   playerService.leavePlayer(socket.id);
 });
 
-router.get("player-quit", (socket: Socket) => {
-  playerService.leavePlayer(socket.id);
-});
+// router.get("player-quit", (socket: Socket) => {
+//   playerService.leavePlayer(socket.id);
+// });
 
 export const PlayerController = router.router;
 
