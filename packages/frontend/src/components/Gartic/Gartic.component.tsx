b24a2c75e1693d56b49b467c111e680cc4faa84f
m4nd4r1n <kara.mandarin99@gmail.com>
2022-11-30 19:18:57 +0900

    feat: 제시어 입력 단계 구현

--- packages/frontend/src/components/Gartic/Gartic.component.tsx
+++ packages/frontend/src/components/Gartic/Gartic.component.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useAtomValue } from "jotai";
 
 import useGartic from "../../hooks/useGartic";
@@ -6,11 +8,15 @@ import {
   GamePageRoundParagraph,
 } from "../../pages/GamePage/GamePage.styles";
 import { gameInfoAtom } from "../../store/game";
+import { socketAtom } from "../../store/socket";
+import { CanvasLayout } from "../Canvas/Canvas.styles";
 import Chat from "../Chat/Chat.component";
 import { Button } from "../common/Button";
+import { Input } from "../common/Input";
 import { Logo } from "../Logo/Logo.component";
 import MoonTimer from "../MoonTimer/MoonTimer.component";
 import PaintBoard from "../PaintBoard/PaintBoard.component";
+import { KeywordInputLayout } from "../PaintBoard/PaintBoard.styles";
 import PlayerList from "../PlayerList/PlayerList.component";
 
 const Gartic = () => {
@@ -24,21 +30,63 @@ const Gartic = () => {
     roundInfo: { currentRound, roundTime },
   } = useGartic();
   const gameInfo = useAtomValue(gameInfoAtom);
+  const socket = useAtomValue(socketAtom);
+  const isDone =
+    garticPlayerList.find((player) => player.peerId === socket.id)?.isDone ??
+    false;
+  const [keywordInput, setKeywordInput] = useState("");
 
+  const onButtonClick = () => {
+    buttonClickMap[gameState]();
+  };
+
+  const keywordButtonClick = () => {
+    if (isDone) {
+      socket.emit("keyword-cancel");
+      return;
+    }
+    socket.emit("input-keyword", { keyword: keywordInput });
+  };
+
+  const onKeywordInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeywordInput(e.target.value);
+  };
+
+  const buttonClickMap = {
+    gameStart: keywordButtonClick,
+    drawing: () => {
+      return;
+    },
+    inputKeyword: () => {
+      return;
+    },
+    gameEnd: () => {
+      return;
+    },
+  };
   const headerElementMap = {
-    gameStart: "",
+    gameStart: "제시어를 입력해주세요",
     drawing: "",
     inputKeyword: "",
     gameEnd: "",
   };
   const centerElementMap = {
-    gameStart: null,
+    gameStart: <CanvasLayout />,
     drawing: null,
     inputKeyword: null,
     gameEnd: null,
   };
   const footerElementMap = {
-    gameStart: null,
+    gameStart: (
+      <KeywordInputLayout>
+        <Input
+          disabled={isDone}
+          variant="medium"
+          placeholder="제시어를 입력하세요."
+          onChange={onKeywordInputChange}
+        />
+      </KeywordInputLayout>
+    ),
     drawing: null,
     inputKeyword: null,
     gameEnd: null,
@@ -67,7 +115,13 @@ const Gartic = () => {
       <GamePageContentBox>
         <MoonTimer radius={50} secondTime={roundTime} />
         <Chat />
-        <Button variant="large">완료</Button>
+        <Button
+          variant="large"
+          onClick={onButtonClick}
+          disabled={!keywordInput}
+        >
+          {isDone ? "편집" : "완료"}
+        </Button>
       </GamePageContentBox>
     </>
   ) : null;
