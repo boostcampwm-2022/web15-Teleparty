c7c5b5941f92ed9cf9a9a05bf572862446440d4d
m4nd4r1n <kara.mandarin99@gmail.com>
2022-12-02 03:24:31 +0900

    feat: 타입 추가
    Co-Authored-By: Yu Jin <62554964+jjiny0716@users.noreply.github.com>
    Co-Authored-By: youhavetopay <58547919+youhavetopay@users.noreply.github.com>
    Co-Authored-By: spaff13 <80938577+spaff13@users.noreply.github.com>

--- packages/backend/src/domains/garticphone/inbound/garticphone.port.ts
+++ packages/backend/src/domains/garticphone/inbound/garticphone.port.ts
@@ -1,6 +1,12 @@
+export type RoundType = "painting" | "keyword";
 export interface GarticphonePort {
   startGame: (roomId: string, roundTime: number, players: string[]) => void;
-  setAlbumData: (roomId: string, playerId: string, data: string) => void;
+  setAlbumData: (
+    roomId: string,
+    playerId: string,
+    data: string,
+    type: RoundType
+  ) => void;
   cancelAlbumData: (roomId: string, playerId: string) => void;
   sendAlbum: (roomId: string, playerId: string) => void;
   exitGame: (roomId: string, playerId: string) => void;
