724e190b72c01f661b512913e3b08ee4c3c403d7
jjiny0716 <jjiny3773@naver.com>
2022-12-01 13:24:03 +0900

    feat: AudioStreamManager에 음향 분석을 위한 초기화 구현
    - add시 음향 분석을 위해 analyser 노드 부착 및 저장하도록 함

--- packages/frontend/src/utils/audioStreamMap.ts
+++ packages/frontend/src/utils/audioStreamMap.ts
@@ -1,26 +1,44 @@
 interface RealTimeAudio {
   stream: MediaStream;
   audio: HTMLAudioElement;
+  audioContext: AudioContext;
+  audioSource: MediaStreamAudioSourceNode;
+  analyser: AnalyserNode;
 }
 
 class AudioStreamManager {
   private map = new Map<string, RealTimeAudio>();
 
   add(id: string, stream: MediaStream) {
-    const audio = new Audio();
-    audio.srcObject = stream;
-    audio.autoplay = true;
-
     if (stream.getAudioTracks().length === 0) {
       console.warn("stream has 0 audio tracks, so nothing will be played.");
     }
 
-    this.map.set(id, {
-      stream,
-      audio,
-    });
+    this.map.set(id, this.streamToRealTimeAudio(stream));
 
     return this;
+  }
+
+  private streamToRealTimeAudio(stream: MediaStream): RealTimeAudio {
+    const audio = new Audio();
+    audio.srcObject = stream;
+    audio.autoplay = true;
+    const audioContext = new AudioContext();
+    const audioSource = audioContext.createMediaStreamSource(stream);
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 512;
+    analyser.minDecibels = -127;
+    analyser.maxDecibels = 0;
+    analyser.smoothingTimeConstant = 0.1;
+    audioSource.connect(analyser);
+
+    return {
+      stream,
+      audio,
+      audioContext,
+      audioSource,
+      analyser,
+    };
   }
 
   remove(id: string) {
