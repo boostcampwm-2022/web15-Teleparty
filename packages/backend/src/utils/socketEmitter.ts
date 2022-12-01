c65b2c3497ce981b00e87335df34505746f82604
spaff13 <spaff316@gmail.com>
2022-12-01 14:12:22 +0900

    feat: 소켓 util함수 데이터 받지 않아도 되게 변경
    Co-authored-by: youhavetopay <youhavetopay@users.noreply.github.com>

--- packages/backend/src/utils/socketEmitter.ts
+++ packages/backend/src/utils/socketEmitter.ts
@@ -4,7 +4,7 @@ export class SocketEmitter {
   static server: Server;
   static sockets: Socket[];
 
-  broadcastRoom(roomId: string, event: string, data: unknown) {
+  broadcastRoom(roomId: string, event: string, data?: unknown) {
     SocketEmitter.server.to(roomId).emit(event, data);
   }
 
