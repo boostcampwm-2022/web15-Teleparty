d0005d826ae236a5ed096d7df1665f90541219d7
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-12-08 01:01:37 +0900

    feat: room.repository 레디스 적용
    - room, searchRoom Service 파일도 수정됨

--- packages/backend/src/domains/room/entity/room.service.ts
+++ packages/backend/src/domains/room/entity/room.service.ts
@@ -1,4 +1,4 @@
-import { Player } from "../../player/entity/player.entitiy";
+import { Player } from "./player.entitiy";
 import { RoomPort } from "../inbound/room.port";
 import { RoomApiAdapter } from "../outbound/room.api.adapter";
 import { RoomEventAdapter } from "../outbound/room.event.adapter";
@@ -8,7 +8,8 @@ import {
   RoomEvent,
 } from "../outbound/room.port";
 import { RoomRepository } from "../outbound/room.repository";
-import { GAME_MODE } from "./room.entity";
+import { GAME_MODE, Room } from "./room.entity";
+import { randomUUID } from "crypto";
 
 export class RoomService implements RoomPort {
   roomRepository: RoomRepositoryDataPort;
@@ -21,140 +22,173 @@ export class RoomService implements RoomPort {
     this.roomEventEmitter = new RoomEventAdapter();
   }
 
+  async createPlayer(data: {
+    peerId: string;
+    userName: string;
+    avata: string;
+    roomId: string | null;
+  }) {
+    const { peerId, userName, avata, roomId } = data;
+
+    let room: Room | undefined = undefined;
+
+    if (roomId) {
+      room = await this.roomRepository.findOneByRoomId(roomId);
+
+      if (!room) {
+        // 유효하지 않는 방 번호
+        room = await this.createRoom();
+      }
+
+      if (room.players.length === room.maxPlayer) {
+        // 가득 참
+        return undefined;
+      }
+
+      if (!room.state) {
+        // 게임중
+        return undefined;
+      }
+    } else {
+      room = await this.createRoom();
+      console.log(room);
+    }
+
+    const player = this.roomRepository.createUser({
+      peerId,
+      userName,
+      avata,
+      roomId: room.roomId,
+    });
+
+    return player;
+  }
+
+  async createRoom() {
+    const newRoomId = await this.createUUID();
+    return this.roomRepository.create(newRoomId);
+  }
+
   // roomId는 playerController에서 처리하기 때문에 무조건 넘어옴
-  async join(peerId: string, roomId: string) {
-    const room = this.roomRepository.findOneByRoomId(roomId);
-    const allPlayer = await this.roomApiAdapter.getAllPlayer();
+  async join(player: Player) {
+    const room = await this.roomRepository.findOneByRoomId(player.roomId);
 
-    if (!allPlayer) {
-      // 접속한 플레이어가 없으면 문제 있는거임 ㅋ
-      return;
-    }
-
-    // 생성된 방이 없는 경우
     if (!room) {
-      const newRoom = this.roomRepository.create(roomId);
-      console.log("room.service newRoom", newRoom.roomId);
-
-      newRoom.host = peerId;
-
-      newRoom.players.push(peerId);
-
-      this.roomEventEmitter.join(
-        {
-          roomId: newRoom.roomId,
-          players: newRoom.players.map((peerId) => {
-            const player = allPlayer.find(
-              (player) => player.peerId === peerId
-            ) as Player;
-
-            return {
-              peerId: player.peerId,
-              userName: player.userName,
-              avataURL: player.avata,
-              isHost: player.peerId === newRoom.host,
-              isMicOn: player.isMicOn,
-            };
-          }),
-        },
-        peerId
-      );
-
-      console.log(
-        "room.service 새로운방 생성 완료",
-        this.roomRepository.findOneByRoomId(roomId)?.roomId
-      );
-
+      // 방이 없음 ?
       return;
     }
 
-    room.players.push(peerId);
+    room.players.push(player.peerId);
 
-    console.log("room.service oldRoom", room.roomId);
-    this.roomEventEmitter.join(
-      {
-        roomId: room.roomId,
-        players: room.players.map((peerId) => {
-          const player = allPlayer.find(
-            (player) => player.peerId === peerId
-          ) as Player;
-
-          return {
-            peerId: player.peerId,
-            userName: player.userName,
-            avataURL: player.avata,
-            isHost: player.peerId === room.host,
-            isMicOn: player.isMicOn,
-          };
-        }),
-      },
-      peerId
-    );
-
-    const playerInfo = allPlayer.find((player) => player.peerId === peerId);
-
-    if (playerInfo) {
+    if (!room.host) {
+      // 내가 방에 처음 들어갔을 경우(내가 방을 만든 경우)
+      room.host = player.peerId;
+    } else {
       this.roomEventEmitter.newJoin(
         {
-          peerId: playerInfo.peerId,
-          userName: playerInfo.userName,
-          avataURL: playerInfo.avata,
-          isHost: playerInfo.peerId === room.host,
-          isMicOn: playerInfo.isMicOn,
+          peerId: player.peerId,
+          userName: player.userName,
+          avataURL: player.avata,
+          isHost: player.peerId === room.host,
+          isMicOn: player.isMicOn,
         },
         room.roomId
       );
     }
 
+    const players = await this.roomRepository.findPlayersByPeerIds(
+      room.players
+    );
+
+    if (!players) {
+      // 방에 플레이어 정보 없음?
+      return;
+    }
+
+    console.log(room.players);
+
+    console.log(players);
+
+    this.roomRepository.save(room.roomId, room);
+
+    this.roomEventEmitter.join(
+      {
+        roomId: room.roomId,
+        players: players.map((roomPlayer) => {
+          return {
+            peerId: roomPlayer.peerId,
+            userName: roomPlayer.userName,
+            avataURL: roomPlayer.avata,
+            isHost: roomPlayer.peerId === room.host,
+            isMicOn: roomPlayer.isMicOn,
+          };
+        }),
+      },
+      player.peerId
+    );
+
     return;
   }
 
   async leave(peerId: string) {
-    const room = this.roomRepository.findOneByPeerId(peerId);
+    const player = await this.roomRepository.findPlayerByPeerId(peerId);
+    if (!player) {
+      return;
+    }
 
+    const room = await this.roomRepository.findOneByRoomId(player.roomId);
     if (!room) {
       return;
     }
-    console.log("room.service leaveRoom", room.roomId, peerId);
 
-    if (room && !room.state) {
+    // 게임 하고 있을 때
+    if (!room.state) {
       this.roomApiAdapter.playerQuit(room.gameMode, room.roomId, peerId);
     }
 
+    // 나밖에 없을 때
     if (room.players.length === 1) {
-      console.log("플레이어 정보", room.players);
-
-      console.log("room.service 방삭제", room.roomId);
       this.roomRepository.deleteByRoomId(room.roomId);
       return;
     }
 
-    if (room.host === peerId) {
-      const newHostPlayer = room.players.find((id) => id !== peerId) as string;
+    // 내가 방장일 때
+    if (room.host === player.peerId) {
+      const newHost = room.players.find((peerId) => {
+        peerId !== room.host;
+      });
 
-      this.roomRepository.updateHostByRoomId(room.roomId, newHostPlayer);
+      // 내가 방장인데 나밖에 없을 때? -> 위에서 걸러지긴 하는데..
+      if (!newHost) {
+        return;
+      }
+
+      this.roomRepository.updateHostByRoomId(room.roomId, newHost);
     }
 
-    this.roomRepository.deletePlayerofRoomByPeerId(peerId);
-
-    const allPlayer = await this.roomApiAdapter.getAllPlayer();
-
-    if (!allPlayer) {
-      // 총 접속한 플레이어가 없을 때 이벤트를 보낼 필요가 없음으로 끝내기
-      return;
-    }
+    this.roomRepository.deletePlayer(peerId, room);
 
     this.roomEventEmitter.quitPlayer(room.roomId, peerId);
 
     return;
   }
-  gameStart(peerId: string, gameMode: GAME_MODE) {
-    const room = this.checkHostByPeerId(peerId);
-    if (room && room.state) {
-      this.roomRepository.updateGameModeByRoomId(room.roomId, gameMode);
+  async gameStart(peerId: string, gameMode: GAME_MODE) {
+    const room = await this.checkHostByPeerId(peerId);
 
-      // 게임이 시작하면 못들어오게 막기
-      this.roomRepository.updateStateByRoomId(room.roomId, false);
+    if (!room) {
+      return;
+    }
+
+    if (room.state) {
+      room.gameMode = gameMode;
+      room.state = false;
+
+      this.roomRepository.save(room.roomId, room);
+
+      // this.roomRepository.updateGameModeByRoomId(room.roomId, gameMode);
+
+      // // 게임이 시작하면 못들어오게 막기
+      // this.roomRepository.updateStateByRoomId(room.roomId, false);
 
       // 게임시작 신호 보내기(game한테)
       this.roomApiAdapter.gameStart(
@@ -169,8 +203,8 @@ export class RoomService implements RoomPort {
 
     return;
   }
-  chooseMode(peerId: string, gameMode: GAME_MODE) {
-    const room = this.checkHostByPeerId(peerId);
+  async chooseMode(peerId: string, gameMode: GAME_MODE) {
+    const room = await this.checkHostByPeerId(peerId);
     if (room) {
       this.roomRepository.updateGameModeByRoomId(room.roomId, gameMode);
 
@@ -186,8 +220,13 @@ export class RoomService implements RoomPort {
     return;
   }
 
-  checkHostByPeerId(peerId: string) {
-    const room = this.roomRepository.findOneByPeerId(peerId);
+  async checkHostByPeerId(peerId: string) {
+    const player = await this.roomRepository.findPlayerByPeerId(peerId);
+    if (!player) {
+      return undefined;
+    }
+
+    const room = await this.roomRepository.findOneByRoomId(player.roomId);
 
     if (room) {
       if (room.host === peerId) {
@@ -200,8 +239,9 @@ export class RoomService implements RoomPort {
     return undefined;
   }
 
-  chatting(peerId: string, message: string) {
-    const room = this.roomRepository.findOneByPeerId(peerId);
+  async chatting(peerId: string, message: string) {
+    const room = await this.roomRepository.findOneByPeerId(peerId);
+    console.log("채팅하는 방", room);
 
     if (room) {
       this.roomApiAdapter.chatting(peerId, room.roomId, message);
@@ -210,8 +250,28 @@ export class RoomService implements RoomPort {
     return;
   }
 
-  endGame(roomId: string) {
-    const room = this.roomRepository.findOneByRoomId(roomId);
+  checkPlayer(peerId: string) {
+    const allPlayer = this.roomRepository.findAllPlayer();
+    if (allPlayer.includes(peerId)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async createUUID() {
+    let uuid = randomUUID();
+
+    while (await this.roomRepository.findOneByRoomId(uuid)) {
+      uuid = randomUUID();
+      console.log("uuid 무한");
+    }
+
+    return uuid;
+  }
+
+  async endGame(roomId: string) {
+    const room = await this.roomRepository.findOneByRoomId(roomId);
     if (room) {
       this.roomRepository.updateStateByRoomId(room.roomId, true);
     }
--- packages/backend/src/domains/room/outbound/room.repository.ts
+++ packages/backend/src/domains/room/outbound/room.repository.ts
@@ -1,75 +1,128 @@
 import { GAME_MODE, Room } from "../entity/room.entity";
-import { RoomRepositoryDataPort } from "./room.port";
+import { NewPlayer, RoomRepositoryDataPort } from "./room.port";
+import { redisCli } from "../../../config/redis";
+import { Player } from "../entity/player.entitiy";
 
 export class RoomRepository implements RoomRepositoryDataPort {
-  private static rooms: Room[] = [];
+  private static players: string[] = [];
 
   create(roomId: string) {
-    const room = new Room(roomId);
-    console.log(RoomRepository.rooms.map((room) => room.roomId));
-    RoomRepository.rooms.push(room);
+    const room = new Room({ roomId });
+
+    this.save(roomId, room);
     return room;
   }
 
-  findOneByRoomId(roomId?: string) {
-    const room = RoomRepository.rooms.find((room) => room.roomId === roomId);
+  createUser(data: NewPlayer) {
+    const { peerId } = data;
+    const newPlayer = new Player(data);
+    RoomRepository.players.push(peerId);
+    redisCli.set(makeHashKeyByPeerId(peerId), JSON.stringify(newPlayer));
+    return newPlayer;
+  }
+
+  save(roomId: string, room: Room) {
+    redisCli.set(makeHashKeyByRoomId(roomId), JSON.stringify(room));
+  }
+
+  findAllPlayer() {
+    return RoomRepository.players;
+  }
+
+  async findOneByRoomId(roomId: string) {
+    console.log("findOneByRoomId ", roomId);
+
+    console.log(makeHashKeyByRoomId(roomId));
+
+    const data = await redisCli.get(makeHashKeyByRoomId(roomId));
+
+    if (!data) return;
+    console.log(data);
+    const room = new Room(JSON.parse(data));
     return room;
   }
 
-  findOneByPeerId(peerId: string) {
-    const room = RoomRepository.rooms.find((room) => {
-      return room.players.includes(peerId);
-    });
+  async findOneByPeerId(peerId: string) {
+    const player = await this.findPlayerByPeerId(peerId);
+    if (!player) {
+      return undefined;
+    }
+    const roomJson = await redisCli.get(makeHashKeyByRoomId(player.roomId));
+    if (!roomJson) {
+      return undefined;
+    }
+
+    const room = new Room(JSON.parse(roomJson));
 
     return room;
   }
 
-  findAll() {
-    return [...RoomRepository.rooms];
+  async findPlayerByPeerId(peerId: string) {
+    const playerJson = await redisCli.get(makeHashKeyByPeerId(peerId));
+
+    if (!playerJson) {
+      return undefined;
+    }
+
+    return new Player(JSON.parse(playerJson));
   }
 
-  updateHostByRoomId(roomId: string, peerId: string) {
-    RoomRepository.rooms.forEach((room) => {
-      if (room.roomId === roomId) {
-        room.host = peerId;
+  async findPlayersByPeerIds(peerIds: string[]) {
+    const players: Player[] = [];
+
+    for (const peerId of peerIds) {
+      const playerJson = await redisCli.get(makeHashKeyByPeerId(peerId));
+
+      if (playerJson) {
+        const player = new Player(JSON.parse(playerJson));
+        players.push(player);
       }
-      return room;
-    });
+    }
+
+    return players;
   }
 
-  updateStateByRoomId(roomId: string, state: boolean) {
-    RoomRepository.rooms.forEach((room) => {
-      if (room.roomId === roomId) {
-        room.state = state;
-      }
-      return room;
-    });
+  async updateHostByRoomId(roomId: string, peerId: string) {
+    const room = await this.findOneByRoomId(roomId);
+    if (!room) return;
+    room.host = peerId;
+    this.save(roomId, room);
   }
 
-  updateGameModeByRoomId(roomId: string, gameMode: GAME_MODE) {
-    RoomRepository.rooms.forEach((room) => {
-      if (room.roomId === roomId) {
-        room.gameMode = gameMode;
-      }
-      return room;
-    });
+  async updateStateByRoomId(roomId: string, state: boolean) {
+    const room = await this.findOneByRoomId(roomId);
+    if (!room) return;
+    room.state = state;
+    this.save(roomId, room);
   }
 
-  deleteByRoomId(roomId: string) {
+  async updateGameModeByRoomId(roomId: string, gameMode: GAME_MODE) {
+    const room = await this.findOneByRoomId(roomId);
+    if (!room) return;
+    room.gameMode = gameMode;
+    this.save(roomId, room);
+  }
+
+  async deleteByRoomId(roomId: string) {
     console.log("delete roomId", roomId);
-    console.log(RoomRepository.rooms.map((room) => room.roomId));
 
-    RoomRepository.rooms = RoomRepository.rooms.filter(
-      (room) => room.roomId !== roomId
-    );
-    console.log(RoomRepository.rooms.map((room) => room.roomId));
+    if (await redisCli.isExists(makeHashKeyByRoomId(roomId)))
+      redisCli.del(makeHashKeyByRoomId(roomId));
   }
 
-  deletePlayerofRoomByPeerId(peerId: string) {
-    RoomRepository.rooms.forEach((room) => {
-      room.players = room.players.filter((id) => {
-        return id !== peerId;
-      });
+  deletePlayer(peerId: string, room: Room) {
+    redisCli.unlink(makeHashKeyByPeerId(peerId));
+    this.save(room.roomId, room);
+    RoomRepository.players = RoomRepository.players.filter((id) => {
+      id !== peerId;
     });
   }
 }
+
+const makeHashKeyByPeerId = (peerId: string): string => {
+  return `player/${peerId}`;
+};
+
+const makeHashKeyByRoomId = (roomId: string): string => {
+  return `room/${roomId}`;
+};
