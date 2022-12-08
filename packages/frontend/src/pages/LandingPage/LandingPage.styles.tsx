5f26f69ed975b6eb7d261d45c000b1b6733765af
m4nd4r1n <kara.mandarin99@gmail.com>
2022-12-09 00:16:36 +0900

    feat: 랜딩 페이지 반응형 적용

--- packages/frontend/src/pages/LandingPage/LandingPage.styles.tsx
+++ packages/frontend/src/pages/LandingPage/LandingPage.styles.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 
-export const LandingPageLayout = styled.main`
+export const LandingPageLayout = styled.main<{ ratio: number }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 100px;
   height: 100vh;
+  transform: scale(${({ ratio }) => ratio});
 `;
