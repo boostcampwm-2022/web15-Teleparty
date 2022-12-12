1c678ffa962ae14d69011908abba2c30b18dc25b
jjiny0716 <jjiny3773@naver.com>
2022-12-12 22:49:47 +0900

    style: MoonTimer 남은 시간 텍스트 스타일링 적용

--- packages/frontend/src/components/MoonTimer/MoonTimer.styles.tsx
+++ packages/frontend/src/components/MoonTimer/MoonTimer.styles.tsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
-export const MoonTimerLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+export const MoonTimerTimeParagraph = styled.p`
+  font-family: "Dongle", sans-serif;
+  font-size: 1rem;
+  scale: 4;
+  color: ${(props) => props.theme.colors.yellow};
 `;
 
 export const MoonTimerCanvas = styled.canvas`
@@ -11,6 +12,12 @@ export const MoonTimerCanvas = styled.canvas`
   rotate: 45deg;
 `;
 
-export const MoonTimerTimeParagraph = styled.p`
-  font-size: 2.2rem;
+export const MoonTimerLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${MoonTimerTimeParagraph} {
+    margin-top: 25px;
+  }
 `;
