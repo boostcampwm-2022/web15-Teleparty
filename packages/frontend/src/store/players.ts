b6474b63ba2dd8ed8e4e548fd79967ba996bb968
jjiny0716 <jjiny3773@naver.com>
2022-12-13 18:59:24 +0900

    refactor: playersAtom 관련 유틸 함수 구현

--- packages/frontend/src/store/players.ts
+++ packages/frontend/src/store/players.ts
@@ -7,3 +7,15 @@ export const playersAtom = atom<GamePlayer[]>([]);
 export const playerIdsAtom = atom((get) =>
   get(playersAtom).map(({ peerId }) => peerId)
 );
+
+export const getPlayerNameById = (players: GamePlayer[], playerId: string) =>
+  players.find(({ peerId }) => peerId === playerId)?.userName;
+
+export const isPlayerHost = (players: GamePlayer[], playerId: string) =>
+  players.find(({ isHost }) => isHost)?.peerId === playerId;
+
+export const isPlayerReady = (players: GamePlayer[], playerId: string) =>
+  players.find(({ peerId }) => peerId === playerId)?.isReady ?? false;
+
+export const isAllPlayerQuitFromGame = (players: GamePlayer[]) =>
+  players.every(({ isGameQuit }) => isGameQuit);
