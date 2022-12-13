6a9030d599edc92595ddcaca4de854292f68d1f8
jjiny0716 <jjiny3773@naver.com>
2022-12-13 13:25:00 +0900

    feat: 방 입장시 서버에 아바타 url 전송

--- packages/frontend/src/pages/LandingPage/LandingPage.component.tsx
+++ packages/frontend/src/pages/LandingPage/LandingPage.component.tsx
@@ -11,6 +11,7 @@ import AvatarChanger from "../../components/AvatarChanger/AvatarChanger.componen
 import { Button } from "../../components/common/Button";
 import { Logo } from "../../components/Logo/Logo.component";
 import NicknameInput from "../../components/NicknameInput/NicknameInput.component";
+import { avatarUrlAtom } from "../../store/avatarUrl";
 import { peerAtom } from "../../store/peer";
 import { playersAtom } from "../../store/players";
 import { ratioAtom } from "../../store/ratio";
@@ -35,6 +36,7 @@ const LandingPage = () => {
   const nicknameRef = useRef<HTMLInputElement>(null);
   const [nicknameError, setNicknameError] = useState(true);
   const ratio = useAtomValue(ratioAtom);
+  const avatarUrl = useAtomValue(avatarUrlAtom);
 
   const invite = new URLSearchParams(window.location.search).get("invite");
 
@@ -45,7 +47,7 @@ const LandingPage = () => {
     // avatar 추가 필요
     socket.emit("join", {
       userName: nicknameRef.current.value,
-      avatar: "",
+      avatar: avatarUrl,
       roomId: invite,
     });
   };
