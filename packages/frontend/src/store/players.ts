2431c24ad2e678ab2d0b9ab19862b7fe45dd4630
jjiny0716 <jjiny3773@naver.com>
2022-12-13 21:56:43 +0900

    refactor: playersAtom 변경을 위한 write-only-atom 구현
    - 플레이어 추가, 제거등의 비즈니스 로직을 컴포넌트에서 제거하기 위해 write-only-atom을 이용해 action 구현

--- packages/frontend/src/store/players.ts
+++ packages/frontend/src/store/players.ts
@@ -1,13 +1,15 @@
 import { atom } from "jotai";
 
-import type { GamePlayer } from "../types/game";
+import type { GamePlayer, Player } from "../types/game";
 
+// atoms
 export const playersAtom = atom<GamePlayer[]>([]);
 
 export const playerIdsAtom = atom((get) =>
   get(playersAtom).map(({ peerId }) => peerId)
 );
 
+// utils
 export const getPlayerNameById = (players: GamePlayer[], playerId: string) =>
   players.find(({ peerId }) => peerId === playerId)?.userName;
 
@@ -19,3 +21,95 @@ export const isPlayerReady = (players: GamePlayer[], playerId: string) =>
 
 export const isAllPlayerQuitFromGame = (players: GamePlayer[]) =>
   players.every(({ isGameQuit }) => isGameQuit === undefined || isGameQuit);
+
+// actions
+export const addPlayerAtom = atom(null, (get, set, player: Player) => {
+  set(playersAtom, [
+    ...get(playersAtom),
+    { ...player, isMicOn: true, isAudioDetected: false },
+  ]);
+});
+
+export const removePlayerAtom = atom(null, (get, set, playerId: string) => {
+  set(
+    playersAtom,
+    get(playersAtom).filter(({ peerId }) => peerId === playerId)
+  );
+});
+
+export const updatePlayersWithPartialPlayerInfoAtom = atom(
+  null,
+  (get, set, partialPlayerInfo: Partial<GamePlayer>) => {
+    set(
+      playersAtom,
+      get(playersAtom).map((player) => ({ ...player, ...partialPlayerInfo }))
+    );
+  }
+);
+
+export const updatePlayerWithPartialPlayerInfoAtom = atom(
+  null,
+  (
+    get,
+    set,
+    {
+      playerId,
+      partialPlayerInfo,
+    }: { playerId: string; partialPlayerInfo: Partial<GamePlayer> }
+  ) => {
+    set(
+      playersAtom,
+      get(playersAtom).map((player) =>
+        player.peerId === playerId
+          ? { ...player, ...partialPlayerInfo }
+          : player
+      )
+    );
+  }
+);
+
+export const quitPlayerFromGameAtom = atom(
+  null,
+  (get, set, playerId: string) => {
+    set(
+      playersAtom,
+      get(playersAtom).map((player) =>
+        player.peerId === playerId
+          ? {
+              ...player,
+              isGameQuit: true,
+              isCurrentTurn: undefined,
+              isReady: undefined,
+            }
+          : player
+      )
+    );
+  }
+);
+
+// 리팩토링 필요!
+export const removePlayersGameData = atom(null, (get, set) => {
+  set(
+    playersAtom,
+    get(playersAtom).map((player) => {
+      delete player.isReady;
+      delete player.isCurrentTurn;
+      delete player.score;
+      if (player.isGameQuit !== undefined) {
+        player.isGameQuit = !player.isGameQuit;
+      }
+      return player;
+    })
+  );
+});
+
+export const setCurrentTurnPlayerReady = atom(null, (get, set) => {
+  set(
+    playersAtom,
+    get(playersAtom).map((player) =>
+      player.isCurrentTurn
+        ? { ...player, isCurrentTurn: false, isReady: true }
+        : player
+    )
+  );
+});
