9a7970a2ce99648bcf024a2fbb824ef39a79ba35
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-12-14 16:12:36 +0900

    chore: 빠진 파일 추가

--- packages/frontend/src/store/chat.ts
+++ packages/frontend/src/store/chat.ts
@@ -3,6 +3,8 @@ import { atom } from "jotai";
 export interface ChatData {
   id: string;
   message: string;
+  avatarURL: string;
+  username: string;
 }
 
 export const chatAtom = atom<ChatData[]>([]);
